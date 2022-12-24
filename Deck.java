509d337ab98eb1c12df278d05b2a29d94a05e626
aycadernek <aydernek@gmail.com>
2022-12-24 23:37:20 +0300

    Create Deck.java
    Deck class created.

---
+++ Deck.java
@@ -0,0 +1,23 @@
+public class Deck {
+    private Card[] deck;
+
+    public Deck(int length) { 
+        if (length != 52) {
+            deck = new Card[length];
+            for (int i = 0; i < deck.length; i++) {
+                deck[i] = new Card("", "");
+            }
+        } else {
+            deck = new Card[52];
+            String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+            String[] suits = {"♠", "♣", "♥", "♦"};
+            int num = 0;
+            for (int i = 0; i < 4; i++) {
+                for (int a = 0; a < 13; a++) {
+                    deck[num] = new Card(suits[i], values[a]);
+                    num++;
+
+                }
+            }
+        }
+    }
